0bc4fdc593042bcb7d8207ba1c3280eb24d2f40b
Diana Jiang <jiang99diana@gmail.com>
2021-11-27 22:28:23 -0500

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map(item => <a key={links} href='#${item}'>{item}</a>)}</nav>;
 }
 
 export default NavBar;
